73500ea66e8587afdf7eaca49c0ad6923807e04d
Joe Thaxton <14jthaxton@gmail.com>
2019-01-06 23:45:23 -0800

    static pages

--- Basis_Challenge/settings.py
+++ Basis_Challenge/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project_name/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'notes/static')]
 # STATICFILES_DIRS = ("/css",)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
